cd20079b0eaea65928282e31976435f2cdd1c9c2
yxx <326454783@qq.com>
2024-07-10 23:31:19 +0800

    69

--- 甄选项目/project/src/api/product/attr/index.ts
+++ 甄选项目/project/src/api/product/attr/index.ts
@@ -1,13 +1,22 @@
-import request from '@/utils/request';
-import type { CategoryResponseData,AttrResponseData} from './type';
-enum Api{
-    C1_URL='/admin/product/getCategory1',
-    C2_URL='/admin/product/getCategory2/',
-    C3_URL='/admin/product/getCategory3/',
-    ATTR_URL='/admin/product/attrInfoList/'
-
+import request from '@/utils/request'
+import type { CategoryResponseData, AttrResponseData } from './type'
+enum Api {
+  C1_URL = '/admin/product/getCategory1',
+  C2_URL = '/admin/product/getCategory2/',
+  C3_URL = '/admin/product/getCategory3/',
+  ATTR_URL = '/admin/product/attrInfoList/',
 }
-export const reqC1=()=>request.get<any,CategoryResponseData>(Api.C1_URL);
-export const reqC2=(category1Id:number|string)=>request.get<any,CategoryResponseData>(Api.C2_URL+category1Id)
-export const reqC3=(category2Id:number|string)=>request.get<any,CategoryResponseData>(Api.C3_URL+category2Id)
-export const reqAttr=(category1Id:number|string,category2Id:number|string,category3Id:number|string)=>request.get<any,any,AttrResponseData>(Api.ATTR_URL+`${category1Id}/${category2Id}/${category3Id}`);
+export const reqC1 = () => request.get<any, CategoryResponseData>(Api.C1_URL)
+export const reqC2 = (category1Id: number | string) =>
+  request.get<any, CategoryResponseData>(Api.C2_URL + category1Id)
+export const reqC3 = (category2Id: number | string) =>
+  request.get<any, CategoryResponseData>(Api.C3_URL + category2Id)
+export const reqAttr = (
+  category1Id: string | number,
+  category2Id: string | number,
+  category3Id: string | number,
+) =>
+  request.get<any, AttrResponseData>(
+    Api.ATTR_URL + `${category1Id}/${category2Id}/${category3Id}`,
+  )
+// export const reqAttr = (category1Id: string | number, category2Id: string | number, category3Id: string | number) => request.get<any, AttrResponseData>(API.ATTR_URL + `${category1Id}/${category2Id}/${category3Id}`);
--- 甄选项目/project/src/api/product/attr/type.ts
+++ 甄选项目/project/src/api/product/attr/type.ts
@@ -1,19 +1,46 @@
-export interface ResponseData{
-    code:number,
-    message:string,
-    ok:boolean
+//分类相关的数据ts类型
+export interface ResponseData {
+    code: number,
+    message: string,
+    ok: boolean
 }
-export interface CategoryObj{
-    id:number|string,
-    name:string,
-    category1Id?:number
+
+//分类ts类型
+export interface CategoryObj {
+    id: number | string,
+    name: string,
+    category1Id?: number,
+    category2Id?: number
 }
-export interface CategoryResponseData extends ResponseData
-{
-    data:CategoryObj[]
+
+//相应的分类接口返回数据的类型
+export interface CategoryResponseData extends ResponseData {
+    data: CategoryObj[]
 }
-export interface AttrValue{
-    id:number,
-    valueName:string,
-    attrId:number
+
+//属性与属性值的ts类型
+
+//属性值对象的ts类型
+export interface AttrValue {
+    id?: number,
+    valueName: string,
+    attrId?: number,
+    flag?:boolean
+
+}
+//存储每一个属性值的数组类型
+export type AttrValueList = AttrValue[];
+//属性对象
+export interface Attr {
+    id?: number,
+    attrName: string,
+    categoryId: number|string,
+    categoryLevel: number,
+    attrValueList: AttrValueList
+}
+//存储每一个属性对象的数组ts类型
+export type AttrList = Attr[];
+//属性接口返回的数据ts类型
+export interface AttrResponseData extends ResponseData {
+    data: Attr[]
 }
